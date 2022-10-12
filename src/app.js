82dbc4e8fa098a692ba2a125f3c9da671f9264b1
jhcamara <jhcamara@gmail.com>
2022-10-12 19:14:57 +0000

    pushing the exercise

--- src/app.js
+++ src/app.js
@@ -8,4 +8,24 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let domainOptions = [];
+  let pronouns = ["the", "thelast", "onemore"];
+  let adjectives = ["wacky", "chubby", "small"];
+  let nouns = ["geek", "undertaker", "instagrammer"];
+  let domains = [".geocities.com", ".cjb.net"];
+
+  for (let p in pronouns)
+    for (let n in nouns)
+      for (let a in adjectives)
+        for (let d in domains)
+          domainOptions.push(
+            pronouns[p] + adjectives[a] + nouns[n] + domains[d]
+          );
+  console.log(domainOptions);
+
+  let outputDomains = "";
+  for (let domain of domainOptions) {
+    outputDomains += `<li class="listItem">${domain}</li>`;
+  }
+  document.querySelector("#finalResults").innerHTML = outputDomains;
 };
